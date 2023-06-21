697829c279cf5ed5df14122d93a68685004f2671
Lucas Leow <leowjianhoong@gmail.com>
2023-06-21 10:43:55 +0800

    Completed Sync & Async JS

--- AJAX/fetch.js
+++ AJAX/fetch.js
@@ -1,33 +1,7 @@
-import {FormData, Request} from "node-fetch";
-import fetch from "node-fetch";
+let msg = await test();
+console.log(msg);
 
-const root = 'http://jsonplaceholder.typicode.com/';
-let uri = root + 'posts';
-
-let form_data = new FormData();
-form_data.append("userId", 3);
-form_data.append("title", 'Title Created');
-form_data.append('body', 'Content Created')
-
-let options = {
-    method: 'POST',
-    mode: 'cors',
-    body: form_data
-}
-
-let req = new Request(uri, options);
-
-fetch(req)
-.then((resp) => {
-    if (resp.ok) {
-        return resp.json();
-    } else {
-        throw new Error("POST fail");
-    }
-})
-.then((json_resp) => {
-    console.log(json_resp);
-})
-.catch((err) => {
-    console.log("Error caught: ", err.message);
-});
+function test() {
+    return new Promise((resolve, reject) => {setTimeout(resolve("rejected"), 1000)})
+  }
+  
