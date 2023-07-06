4893517b7fb3cf8ae16cbba975c1c525988efd0d
Lucas Leow <leowjianhoong@gmail.com>
2023-07-06 10:15:21 +0800

    Completed Lesson 7: View Engine

--- NodeJS_CrashCourse/express-project/app.js
+++ NodeJS_CrashCourse/express-project/app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.listen(3000); // returns instance of server (can save in var for websockets)
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: 'h:/SelfLearning/BackEnd/JavaScript/NodeJS_CrashCourse'});
+    const blogs = [
+        {title: "Yoshi find eggs", snippet: "Reprehenderit aliquip mollit laboris culpa ex nisi."},
+        {title: "Mario find peach", snippet: "Non occaecat nulla occaecat dolore proident velit esse consectetur laborum nisi reprehenderit dolor anim ut."},
+        {title: "How to defeat Bowser", snippet:"Dolor ex est enim eiusmod eiusmod laborum non ipsum laborum esse sint in."}
+    ];
+    res.render('index', {title:"Dynamic Home", blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: 'h:/SelfLearning/BackEnd/JavaScript/NodeJS_CrashCourse'});
+    res.render('about', {title: "Dynamic About"});
 });
 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', {title: "Dynamic Create"});
 });
 
 app.use((req, res) => {
-    res.sendFile('./views/error.html', {root: 'h:/SelfLearning/BackEnd/JavaScript/NodeJS_CrashCourse'});
+    res.status(404).render('error', {title:"Dynamic Error"});
 })
